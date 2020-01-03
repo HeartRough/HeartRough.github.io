604fcccd203662cd8388a3f91292390f540cb7b5
HeartRougn <1809877964@qq.com>
2020-01-03 13:17:26 +0800

    master

---
+++ html_study_record/cool.php
@@ -0,0 +1,10 @@
+<?php
+// $q=isset($_GET['YZH'])? $_GET['YZH']:'';
+// if(is_array($q)){
+//     foreach ($q as $val) {
+//         echo $val.PHP_EOL;
+//     }
+// }
+// echo $_GET['YZH[]'];
+echo "Yes,You are right.".$_POST['how'];
+?>
