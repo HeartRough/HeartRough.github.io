b91999bbea03306ae00bc6244826adee52eb5f6e
HeartRough <1809877964@qq.com>
2020-01-15 10:30:50 +0800

    master

--- html_study_record/cool.php
+++ html_study_record/cool.php
@@ -1,3 +1,18 @@
+<!doctype HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>大佬的玉照</title>
+    <style>
+    body{
+        font-family:"Comic Sans MS", cursive, sans-serif;
+    }
+    mark{
+        background-color:#ddd;
+    }
+    </style>
+</head>
+<body>
 <?php
 $q=isset($_POST['YZH'])? $_POST['YZH']:'';
 if(is_array($q)){
@@ -5,5 +20,9 @@ if(is_array($q)){
         echo "YZH is ".$val."."."<br>";
     }
 }
-echo "Yes,You are right. ".$_POST['how'];
+echo "Yes,You are right. ".$_POST['how']."<br>";
 ?>
+<mark>恭喜你发现了宝藏!!!</mark><br>
+<img src="http://xinke.xyz/pictures/YZH_DL.jpg" alt="xinke.xyz" width="1080" height="1440">
+</body>
+</html>
