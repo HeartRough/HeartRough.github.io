00d2532de970c3dbf029438ed6e45e88020f8edd
HeartRough <1809877964@qq.com>
2020-04-21 15:11:40 +0800

    add demo

---
+++ demo/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+	
+		<!-- <swiper indicator-dots="true";>
+			<swiper-item><image src="../../static/1.jpg" mode="widthFix"></swiper-item>
+			<swiper-item><image src="../../static/2.jpg" mode="widthFix"></swiper-item>
+		</swiper> -->
+		<view>
+			<image src="../../static/1.jpg" mode="widthFix"></image>
+		</view>
+	
+	</view>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	.content{
+		flex:1;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ demo/pages/tabBar/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<!-- #ifdef H5 -->
+		<swiper indicator-dots="true" autoplay="true" interval="3000" style="height:750upx;width:auto;">
+			<swiper-item class="content"><image src="/static/1.jpg" mode="widthFix"></swiper-item>
+			<swiper-item class="content"><image src="/static/2.jpg" mode="widthFix"></swiper-item>
+		</swiper>
+		<!-- #endif -->
+		<button type="primary" @click="toPath_oneday" style="margin-top:10upx;">一日游</button>
+		<view style="flex: 1;flex-direction: row;">
+			<button type="primary" @click="toPath_halfday" style="background-color: #0081FF;">半日游</button>
+			<button type="primary" @click="toPath_teamplay" style="background-color: #0091ff;">游戏组队</button>
+		</view>
+		
+		<button type="primary" @click="toPath_together" style="background-color: #0071ff;">匹配游</button>
+	<view>
+		
+	</view>
+		
+	
+	</view>
+</template>
+
+<script>
+	export default{ 
+		data(){
+			
+		},
+		methods:{
+			toPath_teamplay(){
+				uni.redirectTo({
+					url:"../../teamPlay/teamPlay"
+				})
+				// uni.switchTab({
+				// 	url:"/pages/tabBar/Message/Message"
+				// })
+			},
+			toPath_halfday(){
+				uni.redirectTo({
+					url:"../../halfday/halfday"
+				})
+			}
+		}
+		
+	}
+</script>
+
+<style>
+	.content{
+		flex:1;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
+</style>
