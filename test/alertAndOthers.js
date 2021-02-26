1279354214ef5637dbab5f96d1dd26dcd2f81246
HeartRough <1809877964@qq.com>
2021-02-26 14:36:30 +0800

    a little change
    delete some background pictures

---
+++ test/alertAndOthers.js
@@ -0,0 +1,12 @@
+function aaa(){
+    alert("you click the alert.");
+}
+function confirM(){
+    var a=confirm("please choose anyone");
+    if(a)alert("you choose the OK");
+    else alert("you choose the cancle");
+}
+function prompT(){
+    var a=prompt("Please write anything","");
+    alert("you input "+a);
+}
---
+++ test/alertAndOthersr.js
@@ -0,0 +1,3 @@
+document.getElementById("aaa").onclick=aaa;
+document.getElementById("confirM").onclick=confirM;
+document.getElementById("prompT").onclick=prompT;
